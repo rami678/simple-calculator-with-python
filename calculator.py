a0b71b7aedcf9e8706cf79826ebb173556138ee3
rami678 <71439116+rami678@users.noreply.github.com>
2020-09-17 04:00:34 -0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,31 @@
+
+
+
+number1 = input ("Enter the first number: ")
+number2 = input ("Enter the second number: ")
+
+print ("number1 + number2 =")
+
+addition = (number1 + number2)
+
+print(addition)
+
+print ("number - number2 =")
+
+low = (number1 - number2)
+
+print(low)
+
+print("number1 * number2 =")
+
+_X = (number1 * number2)
+
+print(_X)
+
+print("number1 / number2 =")
+
+quotion = (number1 / number2)
+
+print(quotion)
+
+exit()
