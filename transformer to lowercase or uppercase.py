c4bac7723a1876bfa9c962b633fb6566ae6057e7
rami678 <71439116+rami678@users.noreply.github.com>
2020-09-17 04:19:53 -0700

    Create transformer to lowercase or uppercase.py

---
+++ transformer to lowercase or uppercase.py
@@ -0,0 +1,13 @@
+theword = input("Enter the word you want to transform it: ")
+
+print("in lowercase: ")
+
+theword = theword.lower()
+
+print(theword)
+
+print("in uppercase: ")
+
+theword = theword.upper()
+
+print(theword)
